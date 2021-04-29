559edc4bd861567e48a8221d8ad4bc4ce9f31cfa
femmon <49613439+femmon@users.noreply.github.com>
2021-04-29 13:32:48 +1200

    Implement sign in page

---
+++ sss_frontend/src/pages/SignIn.js
@@ -0,0 +1,38 @@
+import { Button, Divider, InputLabel, TextField, Typography } from "@material-ui/core";
+import { useState } from 'react';
+import styles from "./SignIn.module.css";
+
+export default function SignIn() {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    return (
+        <section className={styles.signInContainer}>
+            <Typography variant="h5">Enter your name to see "Event"</Typography>
+            <Divider />
+
+            <section className={styles.spacing + " " + styles.inputGroup}>
+                <InputLabel htmlFor="name"><Typography variant="h6">Name:</Typography></InputLabel>
+                <TextField
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                    id="name"
+                    variant="outlined"
+                />
+            </section>
+
+            <section className={styles.spacing + " " + styles.inputGroup}>
+                <InputLabel htmlFor="password"><Typography variant="h6">Password:</Typography></InputLabel>
+                <TextField
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                    id="password"
+                    placeholder="Optional"
+                    variant="outlined" />
+            </section>
+
+            <Button variant="contained" color="primary" className={styles.spacing}
+                Sign In
+            </Button>
+        </section>
+    );
+}
