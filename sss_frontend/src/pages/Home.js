49f97c8f910cc981e30963702c909e181e1b4281
jameszu <zqxhjjh@outlook.com>
2021-05-06 12:29:31 +1200

    SSS-001/Homepage Update

--- sss_frontend/src/pages/Home.js
+++ sss_frontend/src/pages/Home.js
@@ -4,7 +4,7 @@ import CalendarPicker from '../components/calendar'
 import styles from './Home.module.css';
 // eslint-disable-next-line
 import Wrapper from './Timetable.js';
-
+import SelectTime from '../components/selectTime'
 export default function Home() {
     return (
         <Container className={styles.Container}>
@@ -12,13 +12,22 @@ export default function Home() {
                 
             </TextField>
             
-            <Typography variant="h5" color='inherit'> 
-                Choose Dates
-            </Typography>
+            
             {/* <Wrapper/> */}
             <div className={styles.midSection}>
-                <CalendarPicker />
-                
+                <div>
+                    <Typography variant="h5" color='inherit'> 
+                        Choose Dates
+                    </Typography>
+                    <CalendarPicker />
+                </div>
+                <div>
+                    <Typography variant="h5" color='inherit'> 
+                        Choose Time
+                    </Typography>
+                    
+                    <SelectTime />
+                </div>
             </div>
 
             <Link to={"/sign-in"}>
