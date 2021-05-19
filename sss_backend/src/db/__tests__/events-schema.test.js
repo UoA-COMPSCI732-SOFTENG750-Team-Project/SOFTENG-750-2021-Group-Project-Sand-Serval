30b0aeca02c9ff0c390f4e978fca2163cf7cd6db
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-05-19 14:41:25 +1200

    Fix a bunch a stuff

--- sss_backend/src/db/__tests__/events-schema.test.js
+++ sss_backend/src/db/__tests__/events-schema.test.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import event from '../events-schema';
+import { event } from '../events-schema';
 
 let mongod;
 let event1, event2;
@@ -15,28 +15,32 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-    const coll = await mongoose.connection.db.createCollection('event');
+    //const coll = await mongoose.connection.db.createCollection('event');
 
     event1 = {
         name: "aefasd",
-        dates: [new Date()],
+        dates: [new Date('2021-05-10')],
         from: new Date(),
         to: new Date()
     };
 
     event2 = {
         name: "Test",
-        dates: [new Date('2021-05-01'), new Date('2021-05-02'), new Date('2021-05-03'), new Date('2021-05-204')],
+        dates: [new Date('2021-05-01'), new Date('2021-05-02'), new Date('2021-05-03'), new Date('2021-05-04')],
         from: new Date('2021-05-21'),
         to: new Date('2019-03-21')
     };
 
-    await coll.insertMany([event1, event2]);
+    const coll = [event1, event2];
+
+    await event.insertMany(coll);
+
+    //await coll.insertMany([event1, event2]);
 });
 
 
 afterEach(async () => {
-    await mongoose.connection.db.dropCollection('breakfasts');
+    await event.deleteMany();
 });
 
 
@@ -53,10 +57,10 @@ it('gets events', async () => {
     expect(events.length).toBe(2);
 
     expect(events[0].name).toBe("aefasd");
-    expect(events[0].dates).toBe([new Date()]);
+    expect(events[0].dates[0]).toEqual(new Date('2021-05-10'));
 
     expect(events[1].name).toBe("Test");
     expect(events[1].dates.length).toBe(4);
-    expect(events[1].from).toBe(new Date('2021-05-21'));
-    expect(events[1].to).toBe(new Date('2019-03-21'));
+    expect(events[1].from).toEqual(new Date('2021-05-21'));
+    expect(events[1].to).toEqual(new Date('2019-03-21'));
 });
